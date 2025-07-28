0b24819ecc4a3c87aecc16a2132499a41caee890
Jason <jsofokleous@googlemail.com>
2025-07-29 00:09:50 +0100

    fix prod build and only do the https spoofing for dev builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,24 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
-    },
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+          host: 'localhost',
+          port: 5173,
+          https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
+          },
+          headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+          },
+        }
+      : undefined,
+  }
 })
